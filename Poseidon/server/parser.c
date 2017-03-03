ece0b866bc11983551192ed53a0d8be521d5efb2
pravez <paul.ravezb@gmail.com>
2017-03-03 13:11:23 +0100

    Modified CMakeLists and started parser

--- Poseidon/server/parser.c
+++ Poseidon/server/parser.c
@@ -1,5 +1,31 @@
-//
-// Created by paubreton on 03/03/17.
-//
-
+#include <stdlib.h>
 #include "parser.h"
+
+
+void parse_config_file(char* file){
+
+}
+
+struct _tvector* _init_tvector(int size){
+    struct _tvector* tvector = malloc(sizeof(struct _tvector));
+    tvector->_size = size;
+    tvector->_vector = malloc(sizeof(struct _tuple) * size);
+
+    return tvector;
+}
+
+void _delete_tvector(struct _tvector* vector){
+
+}
+
+void _add_tuple(struct _tvector* vector){
+
+}
+
+struct _tvector _get_vector(){
+
+}
+
+int _get_value(struct _tvector* vector, char* key){
+
+}
--- Poseidon/server/parser.h
+++ Poseidon/server/parser.h
@@ -1,6 +1,25 @@
 #ifndef POSEIDON_PARSER_H
 #define POSEIDON_PARSER_H
 
+typedef struct _tuple{
+    char* _key;
+    int _value;
+};
 
+typedef struct  _tvector{
+    int _size;
+    struct _tuple* _vector;
+};
+
+struct _tvector config_vector;
+
+void parse_config_file(char* file);
+
+struct _tvector* _init_tvector(int size);
+void _delete_tvector(struct _tvector* vector);
+void _add_tuple(struct _tvector* vector);
+
+struct _tvector _get_vector();
+int _get_value(struct _tvector* vector, char* key);
 
 #endif //POSEIDON_PARSER_H
