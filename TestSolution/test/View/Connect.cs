b8b4234164b123a669bd087cf5e9df0337c90b93
pgaulier <paul.gaulier@free.fr>
2017-03-03 10:52:18 +0100

    connect class

---
+++ TestSolution/test/View/Connect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace test
+{
+	public class Connect
+	{
+		public Socket sock;
+
+		public Connect ()
+		{
+			sock = new Socket (
+				AddressFamily.InterNetwork,
+				SocketType.Stream,
+				ProtocolType.Tcp);
+		}
+	}
+}
+
