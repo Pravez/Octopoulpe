0e495ab680eba8cf12d8ccda5fb78dc455b3c01d
pgaulier <paul.gaulier@free.fr>
2017-03-07 10:42:53 +0100

    connection to the server

--- TestSolution/test/View/Connect.cs
+++ TestSolution/test/View/Connect.cs
@@ -1,19 +1,51 @@
 ﻿using System;
-using System.Net.Sockets;
+using System.Net;
+using System.Net.Sockets;	
 
 namespace test
-{
+{	
+	// The Connect class connects a client to a server at the address and port specified in the affichage.cfg file
 	public class Connect
 	{
 		public Socket sock;
+		public ViewConfig vc;
 
+		// Opens a connection with the server specified in the affichage.cfg file
 		public Connect ()
 		{
 			sock = new Socket (
 				AddressFamily.InterNetwork,
 				SocketType.Stream,
 				ProtocolType.Tcp);
-			
+
+			vc = new ViewConfig ();
+
+			IPAddress addr = vc.getControllerAddress();
+			int port = vc.getControllerPort();
+
+			IPEndPoint iep = new IPEndPoint (addr, port);
+			sock.Connect (iep);
+		}
+
+		// Shuts down the connection
+		public void Disconnect ()
+		{
+			sock.Shutdown (SocketShutdown.Send);
+			sock.Close ();
+		}
+
+		// Sends data to the server
+		public void PushValue (byte[] buffer)
+		{
+			sock.Send (buffer);
+		}
+
+		// Receives data from the server
+		public byte[] PullValue ()
+		{
+			byte[] buffer = new byte[512];
+			sock.Receive (buffer);
+			return buffer;
 		}
 	}
 }
