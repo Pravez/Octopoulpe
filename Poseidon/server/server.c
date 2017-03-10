f4e9d28f86ef37aa86fcd9d1d144062ff917c184
Louise <louise.mouret@enseirb-matmeca.fr>
2017-03-10 10:16:26 +0100

    Multithread version of server

--- Poseidon/server/server.c
+++ Poseidon/server/server.c
@@ -14,14 +14,24 @@
 #define PARAM 2
 #define CLIENTS 5
 
+/*
+ * @function t_read
+ * @brief    function reading input in a thread
+ * @param    [int] newsockfd 
+ * @return   ??
+ */
+/*
+void * t_read(void * arg) {
+  int n;
+  int newsockfd = *((int *) arg);
+  n = read(newsockfd,
+}
+*/
 void * start(void* arg) {
   char buffer[MAX];
-  int sockfd = *((int *) arg);
+  int newsockfd = *((int *) arg);
   int n;
-  struct sockaddr_in cli_addr;
-  int clilen = sizeof(cli_addr);
-  int newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-  if (newsockfd < 0) {perror("ERROR on accept"); exit(1);}
+  
   bzero(buffer, 256);
   while(1) {
     n = read(newsockfd, buffer, 255);
@@ -34,28 +44,23 @@ void * start(void* arg) {
 
 void connexion(int portno){
   int sockfd, newsockfd, clilen;
-  
   struct sockaddr_in serv_addr, cli_addr;
   int i;
-  pthread_t threads[CLIENTS];
+  clilen = sizeof(cli_addr);
   
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {perror("ERROR opening socket"); exit(1);}
   
   bzero((char *) &serv_addr, sizeof(serv_addr));
-
-  
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
   serv_addr.sin_port = htons(portno);
   if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) error("ERROR on binding");
   listen(sockfd, 5);
-  for(i = 0; i< CLIENTS; ++i) {
-    if(pthread_create(&threads[i], NULL, start, &sockfd) < 0) {perror("pthread_create"); exit(1);}
+  while(newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen)){
+      pthread_t thread;
+      if(pthread_create(&thread, NULL, start, &newsockfd) < 0) {perror("pthread_create"); exit(1);}
   }
-  for(i = 0; i < CLIENTS; ++i)
-    pthread_join(threads[i], NULL);
-
 }
 
 int main(int argc, char *argv[]) {
