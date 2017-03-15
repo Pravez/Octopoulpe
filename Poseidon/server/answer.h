a5b08071e32e10f4fa203c886ecb002c1bf92328
PARANT Laurie-anne <lparant@lutte.pedago.ipb.fr>
2017-03-15 11:43:26 +0100

    answer.h added (soon commented)

---
+++ Poseidon/server/answer.h
@@ -0,0 +1,13 @@
+#ifndef POSEIDON_ANSWER_H
+#define POSEIDON_ANSWER_H
+
+char * asw__hello(char * arg);
+char * asw__get_fishes(char * arg);
+char * asw__get_fishes_continuously(char * arg);
+char * asw__ping(char * arg);
+char * asw__log(char * arg);
+char * asw__add_fish(char * arg);
+char * asw__del_fish(char * arg);
+char * asw__start_fish(char * arg);
+
+#endif //POSEIDON_ANSWER_H
