770456c0ad29ef43c265f0cd0d743263ca82e707
PARANT Laurie-anne <lparant@lutte.pedago.ipb.fr>
2017-03-15 11:53:11 +0100

    answer.c added

---
+++ Poseidon/server/answer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include "answer.h"
+
+char * asw__hello(char * arg)
+{
+  return "greetings\n"
+}
+char * asw__get_fishes(char * arg){return NULL;}
+char * asw__get_fishes_continuously(char * arg){return NULL;}
+char * asw__ping(char * arg){return NULL;}
+char * asw__log(char * arg){return NULL;}
+char * asw__add_fish(char * arg){return NULL;}
+char * asw__del_fish(char * arg){return NULL;}
+char * asw__start_fish(char * arg){return NULL;}
