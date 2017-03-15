934981f0332fb596c879d08beae122b8ccca3283
PARANT Laurie-anne <lparant@lutte.pedago.ipb.fr>
2017-03-15 12:01:59 +0100

    doing asw__hello in answer.c

--- Poseidon/server/answer.c
+++ Poseidon/server/answer.c
@@ -5,8 +5,12 @@
 
 char * asw__hello(char * arg)
 {
-  return "greetings\n"
+  if(!strcmp(arg,"\n"))
+    printf("Par défault\n");
+  char * res ="greetings\n";
+  return res;
 }
+
 char * asw__get_fishes(char * arg){return NULL;}
 char * asw__get_fishes_continuously(char * arg){return NULL;}
 char * asw__ping(char * arg){return NULL;}
@@ -14,3 +18,9 @@ char * asw__log(char * arg){return NULL;}
 char * asw__add_fish(char * arg){return NULL;}
 char * asw__del_fish(char * arg){return NULL;}
 char * asw__start_fish(char * arg){return NULL;}
+
+int main(int argc, char * argv[])
+{
+  asw__hello("\n");
+}
+  
