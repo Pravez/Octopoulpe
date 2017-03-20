65c04a823bc3784ffc0b016f60de4f70d2d471f6
PARANT Laurie-anne <lparant@fraiche.pedago.ipb.fr>
2017-03-20 11:19:44 +0100

    asw__hello() is done (without the auxiliar functions needed by aquarium)

--- Poseidon/server/answer.h
+++ Poseidon/server/answer.h
@@ -1,13 +1,16 @@
 #ifndef POSEIDON_ANSWER_H
 #define POSEIDON_ANSWER_H
 
-char * asw__hello(char * arg);
-char * asw__get_fishes(char * arg);
-char * asw__get_fishes_continuously(char * arg);
-char * asw__ping(char * arg);
-char * asw__log(char * arg);
-char * asw__add_fish(char * arg);
-char * asw__del_fish(char * arg);
-char * asw__start_fish(char * arg);
+#define HELLO_SUCCESS 0
+#define HELLO_FAILURE 1
+
+int asw__hello(char * arg, char * res);
+void asw__get_fishes(char * arg, char * res);
+void asw__get_fishes_continuously(char * arg, char * res);
+void asw__ping(char * arg, char * res);
+void asw__log(char * arg, char * res);
+void asw__add_fish(char * arg, char * res);
+void asw__del_fish(char * arg, char * res);
+void asw__start_fish(char * arg, char * res);
 
 #endif //POSEIDON_ANSWER_H
