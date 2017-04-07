5f86b4a6d42a8753812d7ded27fa92750cc0f239
nicvidal <nividal@outlook.fr>
2017-04-07 17:37:20 +0200

    new project Aquarium added with MonoGame

---
+++ Aquarium/Aquarium/Aquarium.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Aquarium
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Aquarium : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Fish fish;
+
+        public Aquarium()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // Initialize the player class
+            fish = new Fish();
+
+            base.Initialize();
+            fish.setGoal(new Vector2(400, 350));
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Load the player resources
+
+            Vector2 playerPosition = new Vector2(25, 25);
+
+            fish.Initialize(Content.Load<Texture2D>("bg"), Content.Load<Texture2D>("magicarpe2"), Content.Load<Texture2D>("magicarpe3"), playerPosition, 100, 100, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            fish.Update();
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Start drawing
+            spriteBatch.Begin();
+
+            // Draw the Player
+            fish.Draw(spriteBatch);
+
+            // Stop drawing
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Aquarium/Aquarium/Fish.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquarium
+{
+    class Fish
+    {
+        private int time = 0;
+        public int width;
+        public int height;
+        public int global_width;
+        public int global_height;
+
+        public Texture2D background;
+
+        // Animation representing the fish
+        public Texture2D Texture1;
+        public Texture2D Texture2;
+
+        // Position of the Fish relative to the upper left side of the screen
+        public Vector2 Position;
+        public Vector2 Goal;
+
+        // State of the fish
+        public bool Displayed;
+
+
+        // Get the width of the fish
+        public int Width
+        {
+            get { return this.width; }
+        }
+
+        // Get the height of the fish
+        public int Height
+        {
+            get { return this.height; }
+        }
+
+        public void Initialize(Texture2D bg, Texture2D texture1, Texture2D texture2, Vector2 position, int width, int height, int g_w, int g_h)
+        {
+            background = bg;
+            Texture1 = texture1;
+            Texture2 = texture2;
+            global_height = g_h;
+            global_width = g_w;
+            this.width = width;
+            this.height = height;
+            Position = position;
+            Displayed = false;
+            Goal = new Vector2(0, 0);
+        }
+
+        public void setGoal(Vector2 goal)
+        {
+            this.Goal = goal;
+        }
+
+        public void Update()
+        {
+            if (Goal.X != -1 && Goal.Y != -1)
+            {
+                if (Position.X > Goal.X)
+                    Position.X = Position.X - 1;
+                else if (Position.X < Goal.X)
+                    Position.X = Position.X + 1;
+
+                if (Position.Y > Goal.Y)
+                    Position.Y = Position.Y - 1;
+                else if (Position.Y < Goal.Y)
+                    Position.Y = Position.Y + 1;
+
+                if (Position.Equals(Goal))
+                    Goal = new Vector2(-1, -1);
+            }
+
+            time = (time + 1) % 20;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(background, new Rectangle(0, 0, global_width, global_height), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0f);
+
+            if (time < 10)
+                spriteBatch.Draw(Texture1, new Rectangle((int)Position.X, (int)Position.Y, width, height), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0f);
+            else
+                spriteBatch.Draw(Texture2, new Rectangle((int)Position.X, (int)Position.Y, width, height), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0f);
+        }
+    }
+}
