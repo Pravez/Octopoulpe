008c1185d627d19ecf03443436cdc1b65650e506
nicvidal <nicvidal@outlook.fr>
2017-04-27 02:22:06 +0200

    Add background and correct size

--- Aquarium/src/sample/Fish.java
+++ Aquarium/src/sample/Fish.java
@@ -48,7 +48,7 @@ public class Fish {
         String [] listFiles = listImage();
         for (String f: listFiles) {
             System.out.println("DEBUG : file in directory : " + f);
-             if (f.equalsIgnoreCase(pictureFile)) {
+             if (f.equalsIgnoreCase(pictureFile) && !f.equalsIgnoreCase("bg")) { //to prevent bug with background
               System.out.println("DEBUG : NO EQUAAAL");
               exist = true;
              }
