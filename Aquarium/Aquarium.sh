a6997dbe5f3ce504d696be102492ee137a407f97
pravez <paul.ravezb@gmail.com>
2017-05-07 16:36:47 +0200

    Updated readme and added script to generated manually java executable

---
+++ Aquarium/Aquarium.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+DIR=Aquarium_production
+
+usage(){
+    echo "Usage : -h or --help to print this help menu"
+    echo "Output directory is : $DIR/"
+    echo "Use --create option to create and compile sources in output directory"
+    echo "Use --run to run the application"
+    echo "Use --clean to remove output directory and its content"
+    echo "/!\ DO NOT /!\ move output directory, or if you move it, move the folder resources/ relatively to it"
+}
+
+create(){
+    echo "Creating production directory, or cleaning ..."
+    mkdir -p  $DIR/
+    rm -rf $DIR/*
+
+    echo "Compiling java sources ..."
+    javac -d $DIR/ -cp $DIR/ src/sample/*.java
+
+    echo "Copying images ..."
+    cp -r src/sample/Images $DIR/sample/
+
+    echo "Done !"
+}
+
+run(){
+    java -cp $DIR/ sample.Aquarium
+}
+
+clean(){
+    rm -rf $DIR
+}
+
+if [ $# -ge 1 ]
+then
+    while [[ $# -ge 1 ]]
+    do
+    key="$1"
+
+
+    case $key in
+        --create)
+        create
+        shift
+        ;;
+        --run)
+        run
+        shift
+        ;;
+        -h|--help)
+        usage
+        shift
+        ;;
+        --clean)
+        clean
+        shift
+        ;;
+        *)
+                echo "Unknown option"
+        ;;
+    esac
+    shift # past argument or value
+    done
+else
+    echo "Please give at least one option"
+    usage
+fi
--- README.md
+++ README.md
@@ -20,3 +20,16 @@ Entrez dans `build/` et entrez la commande `make all`.
 Il ne vous reste plus qu'à exécuter `build/Poseidon/src/Server` pour lancer le serveur principal !
 Un script réalise toutes ces actions pour vous...
 Il ne vous reste qu'à lancer `./Oceans.sh` pour réaliser tout ceci !
+
+Attention, le fichier `controller.cfg` doit obligatoirement se trouver dans le même répertoire que l'exécutable du serveur. (géré automatiquement par CMake)
+
+## Aquarium
+
+Pour compiler, vous devez posséder l'extension Java de CMake (normalement packagée par défaut dans votre installation de CMake).
+Si malheureusement CMake refuse de compiler correctement, vous pourrez trouver à la racine du projet Aquarium/, un script qui vous
+permettra tout de même de compiler et d'exécuter le programme en Java (même s'il est tout de même plus "agréable" d'utiliser le CMake).
+
+## Note
+
+Les deux scripts fournis disposent d'une fonction `usage`, que vous pouvez appeller à l'aide de l'option -h ou --help, n'hésitez pas à 
+vous en servir...
